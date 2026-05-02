2e45647aa78aeeb0ba1dc550e583fa66bc5d1235
Douglas Finlay <douglas@douglasfinlay.com>
2026-05-02 22:20:09 +0100

    Enable snacks.nvim dashboard

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -3,6 +3,7 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
+        dashboard = { enabled = true },
         input = { enabled = true },
         notifier = { enabled = true },
     },
