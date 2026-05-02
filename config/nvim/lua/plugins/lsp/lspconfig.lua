ecd57f2603fc9c3d228c501fe7c67b48c9266e0c
Douglas Finlay <douglas@douglasfinlay.com>
2026-05-02 21:24:33 +0100

    Migrate from neodev to lazydev

--- config/nvim/lua/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -7,7 +7,6 @@ return {
     dependencies = {
         'hrsh7th/cmp-nvim-lsp',
         { 'antosha417/nvim-lsp-file-operations', config = true },
-        { 'folke/neodev.nvim', opts = {} },
     },
     config = function()
         local lspconfig = require('lspconfig')
